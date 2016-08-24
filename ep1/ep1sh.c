d9666929621d3fade8c0d9dddd995ad5daf1d73a
Victor Sena Molero <victorsenam@gmail.com>
2016-08-24 20:00:29 -0300

    Final da implementação do ep1sh
    Mudanças no commit
    ===================
    
    - Melhor tratamento de erros no execve
    - Implementação do chmod
    - Implementação do id -u

--- ep1/ep1sh.c
+++ ep1/ep1sh.c
@@ -4,6 +4,8 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
 
 #define PROMPT_SIZE (1<<10)
 
@@ -48,24 +50,51 @@ int main (int argc, char * argv[]) {
     char prompt[PROMPT_SIZE];
     pid_t pid;
     int call_status;
+    mode_t chmod_mode;
     char running = 1;
 
     while (running) {
         build_prompt_string(prompt);
         command = readline(prompt);
-        add_history(command);
+        if (command == NULL) {
+            command = (char *) malloc(sizeof(char) * 5);
+            strcpy(command, "exit");
+        } else {
+            add_history(command);
+        }
 
         c_argv = (char **) malloc(sizeof(char *) * count_arguments(command));
         build_call(command, c_argv);
+        call_status = 0;
 
         if (strcmp(c_argv[0], "exit") == 0) {
             running = 0;
+        } else if (strcmp(c_argv[0], "chmod") == 0) {
+            chmod_mode = strtol(c_argv[1], 0, 8);
+            if (chmod(c_argv[2], chmod_mode))
+                fprintf(stderr, "chmod failed: %s\n", strerror(errno));
+        } else if (strcmp(c_argv[0], "id") == 0 && strcmp(c_argv[1], "-u") == 0) {
+            printf("%d\n", getuid());
+        } else if (access(c_argv[0], F_OK)) {
+            fprintf(stderr, "ep1sh: %s is not accessible\n", c_argv[0]);
+        } else if (access(c_argv[0], X_OK)) {
+            fprintf(stderr, "ep1sh: %s is not executable\n", c_argv[0]);            
         } else if ((pid = fork()) == 0) {
-            call_status = execve(c_argv[0], c_argv, c_arge);
-        }
-
-        if (running) {
-            waitpid(pid, &call_status, 0);
+            execve(c_argv[0], c_argv, c_arge);
+            call_status = -1;
+            return -1;
+        } else if (running) {
+            if (call_status == -1) {
+                fprintf(stderr, "ep1sh: failed to execute %s\n", c_argv[0]);
+            } else {
+                waitpid(pid, &call_status, 0);
+                if (WIFEXITED(call_status)) {
+                    if (WEXITSTATUS(call_status))
+                        fprintf(stderr, strerror(errno));
+                } else {
+                    fprintf(stderr, "child process was not terminated normally\n");
+                }
+            }
         }
 
         free(c_argv);
