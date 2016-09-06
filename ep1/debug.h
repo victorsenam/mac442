72d8e718b0985ae4da0b7807c5f95a0c9ec7a40d
Nathan <nathan@vieiraproenca.com>
2016-09-06 07:39:01 -0300

    Adding timestamp debug info

--- ep1/debug.h
+++ ep1/debug.h
@@ -2,9 +2,12 @@
 #define DEBUG_MAC442
 
 #include <stdio.h>
+#include <time.h>
 
 int debug_flag;
 
-#define debug(...) {if (debug_flag) fprintf(stderr, __VA_ARGS__);}
+void debug_print_time ();
+
+#define debug(...) {if (debug_flag) {debug_print_time(); fprintf(stderr, __VA_ARGS__);}}
 
 #endif
