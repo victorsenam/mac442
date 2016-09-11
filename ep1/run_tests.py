0019745c47d171336a535186bbb7fa77497a331a
Victor Sena Molero <victorsenam@gmail.com>
2016-09-11 19:20:24 -0300

    Small python test running script

---
+++ ep1/run_tests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import subprocess
+
+scheduler = 1
+
+while scheduler <= 3 :
+    for curr_type in [ 'small', 'medium', 'large' ]:
+        cnt = 0
+
+        outfile = 'output/' + str(scheduler) + '_' + str(curr_type) + '.out'
+
+        subprocess.call('echo "" > ' + outfile, shell=True)
+        
+        while cnt < 30 :
+            subprocess.call('./testing/gen ' + str(cnt) + ' < testing/' + str(curr_type) + '.in > tmp_input', shell=True)
+            subprocess.call('./ep1 ' + str(scheduler) + ' tmp_input out >> ' + outfile, shell=True)
+            print(str(scheduler) + ' ' + str(curr_type) + ' ' + str(cnt))
+
+            cnt += 1
+    scheduler += 1
+
