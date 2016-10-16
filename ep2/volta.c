dea2250dbda9b882ed28b196440a8e006564c244
Victor Sena Molero <victorsenam@gmail.com>
2016-10-16 03:06:07 -0200

    Bugfix em volta.c

--- ep2/volta.c
+++ ep2/volta.c
@@ -4,8 +4,11 @@ char volta_atual_atualiza () {
     int i, j;
     char cond = 1;
 
+    if (volta_atual == 16) 
+        return 0;
+
     while (cond && volta_atual_barreira < ciclista_n*2) {
-        i = (volta_atual_barreira < ciclista_n);
+        i = (volta_atual_barreira >= ciclista_n);
         j = (volta_atual_barreira - i*ciclista_n);
 
         if (!ciclista[i][j].fim && ciclista[i][j].volta < volta_atual)
