64efa8ab96e8d143b7bf6a16063adad052a4e0b5
Victor Sena Molero <victorsenam@gmail.com>
2016-11-08 14:06:30 -0200

    Readline

--- ep3/ep3.cpp
+++ ep3/ep3.cpp
@@ -1,6 +1,29 @@
+#include <readline/readline.h>
+#include <readline/history.h>
+
 #include <iostream>
 #include "trace.h"
 
+std::string split (std::string & a, char sep) {
+    int pos = a.find(sep);
+    int n = a.length();
+    std::string ret = a.substr(0, pos);
+    a = a.substr(pos+1, n-pos-1);
+    return ret;
+}
+
 int main (int argc, char * argv[]) {
-    std::cout << "ok" << std::endl;
+    while (true) {
+        std::string line = readline("(ep3): ");
+
+        if (line == "sai")
+            break;
+
+        add_history(line.c_str());
+
+        std::string command = split(line, ' ');
+
+        std::cout << command << std::endl;
+        std::cout << line << std::endl;
+    }
 }
--- ep3/trace.cpp
+++ ep3/trace.cpp
@@ -3,16 +3,18 @@
 #include "trace.h"
 
 void read_trace (std::string file_name) {
+    process_v.clear();
+    task_qt = 0;
+
     std::fstream trace(file_name, std::ios::in);
 
     trace >> memory_total >> memory_virtual >> memory_block >> memory_page;
     trace.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-    for (std::string line; std::getline(trace, line);) {
+    std::string line;
+    while (std::getline(trace, line);)
         process_v.push_back(process(line, process_v.size()));
-    }
 
-    for (process pr : process_v) {
+    for (process pr : process_v)
         pr.print();
-    }
 }
